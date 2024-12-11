d6afe86ed6c10e97f7eebaf78f9a9e5128d17ffa
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2024-12-11 22:25:57 +0100

    style: use same for p1 and p2

--- 2024/11/p1.ts
+++ 2024/11/p1.ts
@@ -1,20 +1,22 @@
 function blink(stones: Map<number, number>) {
+  const m = new Map<number, number>();
   for (const [n, times] of stones.entries()) {
     if (n === 0) {
-      stones.set(1, (stones.get(1) ?? 0) + times)
+      const k = m.get(1) ?? 0;
+      m.set(1, k + times)
     } else if (n.toString().length % 2 == 0) {
       const s = n.toString();
       const length = s.length / 2;
       const left = parseInt(s.substring(0, length))
       const right = parseInt(s.substring(length, length + length))
 
-      stones.set(left, (stones.get(left) ?? 0) + times)
-      stones.set(right,(stones.get(right) ?? 0) + times)
+      m.set(left, (m.get(left) ?? 0) + times)
+      m.set(right,(m.get(right) ?? 0) + times)
     } else {
-      stones.set(n * 2024, (stones.get(n * 2024) ?? 0) + times)
+      m.set(n * 2024, (m.get(n * 2024) ?? 0) + times)
     }
   }
-  return stones 
+  return m
 }
 
 export const solve = (input: string) => {
@@ -32,7 +34,7 @@ export const solve = (input: string) => {
     blinked = blink(blinked);
   }
   let n = 0;
-  for (const times of blinked.values()) {
+  for (const [_, times] of blinked.entries()) {
     n += times;
   }
   return n;
