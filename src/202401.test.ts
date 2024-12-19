1245617035ec27c25a014278b5e992cbeaccd6c8
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2024-12-19 17:51:11 +0100

    feat(202401): solve 2024 day 1

---
+++ src/202401.test.ts
@@ -0,0 +1,99 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import { distance, p1, p2, parse } from "./202401.js";
+import { read } from "./lib/main.js";
+
+describe(import.meta.filename, () => {
+  describe(parse.name, () => {
+    it("should parse input", () => {
+      let input = `123   456
+789   912
+345   678`
+
+      const locations = parse(input);
+
+      assert.deepStrictEqual(locations.left, [123, 345, 789]);
+      assert.deepStrictEqual(locations.right, [456, 678, 912]);
+    });
+  });
+
+  describe(p1.name, () => {
+    it("example 1", () => {
+      const example = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+      const n = p1(parse(example));
+
+      assert.strictEqual(n, 11);
+    });
+
+    it("input", async () => {
+      const input = await read("202401");
+
+      const n = p1(parse(input));
+
+      assert.strictEqual(n, 2264607);
+    });
+
+    it("input huge", async () => {
+      const input = await read("202401");
+      const huge = input.repeat(100).trim();
+
+      const n = p1(parse(huge));
+
+      assert.strictEqual(n, 226460700);
+    });
+
+    describe(distance.name, () => {
+      const tests: [left: number, right: number, expected: number][] = [
+        [3, 1, 2],
+        // Added data where left location is lower than right.
+        [2, 7, 5],
+      ];
+
+      for (const [left, right, expected] of tests) {
+        it(`should have distance ${expected} between ${left} and ${right}`, () => {
+          const result = distance(left, right);
+
+          assert.strictEqual(result, expected);
+        });
+      }
+    })
+  });
+
+  describe(p2.name, () => {
+    it("example 1", () => {
+      const example = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+      const n = p2(parse(example));
+
+      assert.strictEqual(n, 31);
+    });
+
+    it("input", async () => {
+      const input = await read("202401");
+
+      const n = p2(parse(input));
+
+      assert.strictEqual(n, 19457120);
+    });
+
+    it("input huge", async () => {
+      const input = await read("202401");
+      const huge = input.repeat(100).trim();
+
+      const n = p2(parse(huge));
+
+      assert.strictEqual(n, 194571200000);
+    });
+  })
+});
---
+++ src/202401.ts
@@ -0,0 +1,120 @@
+import { Main } from "./lib/main.js";
+import assert from "node:assert";
+import { EOL } from "os";
+
+Main(parse, p1, p2, import.meta.url);
+
+export type Locations = {
+  left: number[];
+  right: number[];
+};
+
+export function parse(input: string): Locations {
+  const left: number[] = [];
+  const right: number[] = [];
+
+  const lines = input.trim().split(EOL);
+  for (const line of lines) {
+    const numbers = line.match(/\d+/g)?.map(Number); assert(numbers && numbers.length === 2);
+    const a = numbers[0]; assert.ok(a);
+    const b = numbers[1]; assert.ok(b);
+
+    left.push(a);
+    right.push(b);
+  }
+
+  assert.strictEqual(left.length, right.length);
+
+  // Sort the locations. Both solutions depend on this.
+  left.sort()
+  right.sort()
+
+  return { left, right };
+}
+
+/**
+  * Solve for the total distance between all location ids
+  *
+  * Compare location ids from smallest to biggest. The `Locations` object is
+  * expected to be sorted. With that given we can loop through both lists at
+  * the same time and calculate distances between the location ids.
+  *
+  * @time O(n)
+  * @space O(1)
+  */
+export function p1(locations: Locations): number {
+  let n = 0;
+
+  let i = 0;
+  while (i < locations.left.length && i < locations.right.length) {
+    n += distance(locations.left[i], locations.right[i]);
+    i += 1;
+  }
+
+  return n;
+};
+
+/**
+  * Calculates distance between `left` and `right` location.
+  */
+export function distance(left: number, right: number): number {
+  // Use `Math.abs` in case left location is lower than the right location.
+  return Math.abs(left - right);
+}
+
+/**
+  * Solve the total similarities.
+  *
+  * Loop through both lists and count how many times the `left` location id is
+  * found in the `right` location list.
+  *
+  * @time O(n + m)
+  * @space O(1)
+  */
+export function p2(locations: Locations): number {
+  let n = 0;
+
+  let i = 0;
+  let j = 0;
+  let previous = 0;
+  while (i < locations.left.length) {
+    const location = locations.left[i];
+
+    // The actual input doesn't have this, but the examples do. Here we check
+    // if the location is the same as the previous. Since we use two pointers
+    // in the list we already handled all matching values in the right list. So
+    // when we are the same as the previous just add the previous value again
+    // and continue to next value.
+    //
+    // @example: 
+    // left: [3, 3, 5]
+    // right: [3, 3, 6]
+    //
+    // After handling the first 3 the j pointer is at 6.
+    if (location == locations.left[i - 1]) {
+      i += 1;
+      n += previous;
+      continue;
+    }
+
+    // Lists are sorted, so if the location is greater than the current right
+    // value, we can skip.
+    while (location > locations.right[j]) {
+      j += 1;
+    }
+
+    // Similarity is location * found instances in right list. Which is the
+    // same as keep adding the location on matching locations.
+    let simularity = 0;
+    while (location === locations.right[j]) {
+      simularity += location
+      j += 1;
+    }
+    n += simularity;
+    previous = simularity;
+
+    i += 1;
+  }
+
+  return n;
+};
